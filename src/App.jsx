19995ee47948bea34919fa5cc0ce40a83dc372db
kushagr <kushagr@kushagrs-MacBook-Air.local>
2024-06-15 11:24:55 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+ import Navbar from './components/Navbar';
+ import Hero from './components/Hero';
+ import Highlights from './components/Highlights';
+ import Model from './components/Model'; 
+ import Features from './components/Features';
+
+import * as Sentry from '@sentry/react'
+import HowitWorks from './components/HowitWorks';
+import Footer from './components/Footer';
+ 
+ const App = () => {
+  
+  return (
+    <main className="bg-black">
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      <Model/>
+      <Features/>
+      <HowitWorks/>
+      <Footer/>
+    </main>
+  )
+}
+
+export default App
